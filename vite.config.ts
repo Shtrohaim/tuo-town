4979e4eea7104108bda5084fa146ffe910f3c2d6
Deorel <Deorel251098@gmail.com>
2023-07-18 21:24:59 +0700

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8000,
+    host: '0.0.0.0'
+  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `
+  //             @import "@/assets/scss/mixins.scss";
+  //             @import "@/assets/scss/variables.scss";
+  //           `
+  //     }
+  //   }
+  // },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: ['*', '.js', '.ts', '.vue', '.json']
+  }
+})
