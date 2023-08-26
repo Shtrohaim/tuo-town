a51ef3cca28e9b4361e761a0d74fa4736318fa95
Deorel <Deorel251098@gmail.com>
2023-08-26 20:04:54 +0700

    feat: add custom slider

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <the-header class="header"></the-header>
-  <base-slider></base-slider>
-   <nav>
+  <base-slider
+    class="slider"
+    :havePagination="true"
+    :width="deviceWidth"
+    :carousel="true"
+    :height="100"
+  >
+    <div
+      v-for="slide in data"
+      :key="slide.id"
+      class="slider__slide slider__slide--active"
+      :style="{ width: deviceWidth + 'px' }"
+    >
+      <img class="slider__background" :src="slide.image" :alt="slide.title" />
+      <p class="slider__title h3">{{ slide.title }}</p>
+      <p class="slider__description p_hg">{{ slide.description }}</p>
+      <a v-if="slide?.link" class="slider__link p_hg">Подробнее</a>
+    </div>
+  </base-slider>
+  <nav>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
   </nav>
@@ -12,12 +30,154 @@
 import { RouterLink, RouterView } from 'vue-router'
 import TheHeader from '@/components/TheHeader.vue'
 import baseSlider from '@/components/base/baseSlider.vue'
+import { ref } from 'vue'
+
+const deviceWidth = window.innerWidth
+
+const data = [
+  {
+    id: 0,
+    image: 'src/assets/images/1.jpg',
+    title: 'Исключительное качество без компромиссов',
+    description: 'Ножи «Tuotown» – это главный инструмент поваров и секрет кулинарного мастерства',
+    link: '#'
+  },
+  {
+    id: 1,
+    image: 'src/assets/images/1.jpg',
+    title: 'Слайд 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+  },
+  {
+    id: 2,
+    image: 'src/assets/images/1.jpg',
+    title: 'Слайд 3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    link: '#'
+  },
+  {
+    id: 3,
+    image: 'src/assets/images/1.jpg',
+    title: 'Слайд 4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+  },
+  {
+    id: 4,
+    image: 'src/assets/images/1.jpg',
+    title: 'Слайд 5',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+  },
+  {
+    id: 5,
+    image: 'src/assets/images/1.jpg',
+    title: 'Слайд 6',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+    link: '#'
+  },
+  {
+    id: 6,
+    image: 'src/assets/images/1.jpg',
+    title: 'Слайд 7',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+  }
+]
 </script>
 
-<style>
+<style lang="scss">
 .header {
-  position: sticky;
+  position: fixed;
   top: 0;
-}
 
+  z-index: 9999;
+}
+.slider {
+  &__slide {
+    position: relative;
+    width: 100%;
+    flex-shrink: 0;
+
+    padding: 75px 15px;
+
+    &::before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      display: block;
+      content: '';
+
+      background: linear-gradient(180deg, #000 45%, rgba(0, 0, 0, 0) 100%);
+
+      width: 100%;
+      height: 80vh;
+      z-index: 2;
+    }
+  }
+
+  &__background {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+    object-fit: cover;
+
+    width: 100%;
+    height: 63vh;
+    z-index: -1;
+  }
+
+  &__title {
+    position: relative;
+    z-index: 3;
+
+    color: $white;
+    text-transform: uppercase;
+
+    margin-bottom: 15px;
+  }
+
+  &__description {
+    position: relative;
+    color: $white;
+
+    font-weight: 400;
+    line-height: 140%;
+
+    max-width: 215px;
+
+    margin-bottom: 8px;
+
+    z-index: 3;
+  }
+
+  &__link {
+    position: relative;
+    display: inline-block;
+
+    z-index: 3;
+    color: $white;
+    font-weight: 600;
+    text-transform: uppercase;
+
+    border: 2px solid $red-active;
+    padding: 16px 34px;
+    border-radius: 27px;
+
+    transition: 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      border-radius: 0;
+    }
+
+    &:active {
+      color: $red-active;
+    }
+  }
+}
 </style>
--- src/components/base/baseSlider.vue
+++ src/components/base/baseSlider.vue
@@ -1,32 +1,180 @@
 <template>
-<div class="slider">
-  <div class="slider__container">
-    <div class="slider__content">
-      <slot></slot>
-    </div>
-    <div class="slider__pagination">
-      <div class="slider__pagination-button slider__pagination-button--prev">
-        <svg class="icon24 fill-white">
-          <use href="@/assets/images/svg/navArrowIcon.svg#icon"></use>
-        </svg>
+  <div class="slider">
+    <div class="slider__container" :style="{ height: props.height + 'vh' }">
+      <div
+        ref="sliderContent"
+        draggable="true"
+        class="slider__content"
+        @dragstart="touchStartMethod"
+        @touchstart="touchStartMethod"
+        :style="{
+          transform: `translate3d(${translateX}px,0,0)`,
+          transition: transitionTime + 'ms'
+        }"
+      >
+        <slot></slot>
       </div>
-      <ul class="slider__pagination-list">
-        <li class="slider__pagination-list-item slider__pagination-list-item--active">1</li>
-        <li class="slider__pagination-list-item">2</li>
-        <li class="slider__pagination-list-item">3</li>
-      </ul>
-      <div class="slider__pagination-button slider__pagination-button--next">
-        <svg class=" icon24 fill-white">
-          <use href="@/assets/images/svg/navArrowIcon.svg#icon"></use>
-        </svg>
+      <div v-if="havePagination" class="slider__pagination">
+        <button
+          class="slider__pagination-button slider__pagination-button--prev"
+          @click="pagination(slide - 1)"
+        >
+          <svg class="icon24 fill-white">
+            <use href="@/assets/images/svg/navArrowIcon.svg#icon"></use>
+          </svg>
+        </button>
+        <ul class="slider__pagination-list">
+          <li
+            v-for="item in filteredItems"
+            :key="item"
+            @click="pagination(carousel ? item + 1 : item)"
+            class="slider__pagination-list-item"
+            :class="{
+              'slider__pagination-list-item--active':
+                (item == slide - 1 && carousel) || (item == slide && !carousel)
+            }"
+          >
+            {{ item }}
+          </li>
+        </ul>
+        <button
+          class="slider__pagination-button slider__pagination-button--next"
+          @click="pagination(slide + 1)"
+        >
+          <svg class="icon24 fill-white">
+            <use href="@/assets/images/svg/navArrowIcon.svg#icon"></use>
+          </svg>
+        </button>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
 
+const props = defineProps({
+  havePagination: Boolean,
+  width: Number,
+  carousel: Boolean,
+  height: Number
+})
+
+const slide = props.carousel ? ref(2) : ref(1)
+const slides = ref(1)
+const width = props.width ? props.width : 320
+const translateX = props.carousel ? ref(-width) : ref(0)
+const transitionTime = ref(0)
+const posInit = ref(0)
+const posX1 = ref(0)
+const posX2 = ref(0)
+const posFinal = ref(0)
+const posThreshold = width * 0.35
+const sliderContent = ref()
+const inTransition = ref(false)
+
+const pagination = (newSlide: number) => {
+  if (!inTransition.value) {
+    transitionTime.value = 400
+    slide.value = newSlide
+    translateX.value = 0 - width * (newSlide - 1)
+    inTransition.value = true
+
+    setTimeout(() => {
+      inTransition.value = false
+    }, 402)
+  }
+}
+
+const filteredItems = computed(() => {
+  let begin = 1
+  let step = window.innerWidth <= 640 ? 3 : 6
+  let end = step
+  if (slide.value - 1 >= step && props.carousel) {
+    begin = slide.value > slides.value ? slide.value - 3 : slide.value - 2
+    end = slide.value + 1 > slides.value ? slides.value : slide.value
+  }
+  if (slide.value >= step && !props.carousel) {
+    begin = slide.value >= slides.value ? slide.value - 2 : slide.value - 1
+    end = slide.value + 1 > slides.value ? slides.value : slide.value + 1
+  }
+  return Array.from({ length: end - begin + 1 }, (value, index) => begin + index)
+})
+const touchStartMethod = (event: any) => {
+  if (!inTransition.value) {
+    posInit.value = posX1.value = event.changedTouches
+      ? event.changedTouches[0].clientX
+      : event.clientX
+
+    transitionTime.value = 0
+
+    addEventListener('touchmove', swipeAction)
+    addEventListener('touchend', swipeEnd)
+    addEventListener('drag', swipeAction)
+    addEventListener('dragend', swipeEnd)
+  }
+}
+
+const swipeAction = (event: any) => {
+  let clientX = event.changedTouches ? event.changedTouches[0].clientX : event.clientX
+  posX2.value = posX1.value - clientX
+  posX1.value = clientX
+
+  translateX.value = translateX.value - posX2.value
+}
+
+const swipeEnd = () => {
+  posFinal.value = posInit.value - posX1.value
+
+  removeEventListener('touchmove', swipeAction)
+  removeEventListener('mousemove', swipeAction)
+  removeEventListener('touchend', swipeEnd)
+  removeEventListener('mouseup', swipeEnd)
+
+  if (Math.abs(posFinal.value) > posThreshold) {
+    if (posInit.value < posX1.value) {
+      slide.value--
+    } else if (posInit.value > posX1.value) {
+      slide.value++
+    }
+  }
+  if (posInit.value !== posX1.value) {
+    pagination(slide.value)
+  }
+}
+
+onMounted(() => {
+  slides.value = sliderContent.value.children.length
+  if (props.carousel) {
+    let carouselBegin = sliderContent.value.children[slides.value - 1].cloneNode(true)
+    let carouselEnd = sliderContent.value.children[0].cloneNode(true)
+    sliderContent.value.append(carouselEnd)
+    sliderContent.value.prepend(carouselBegin)
+  }
+})
+
+watch(slide, () => {
+  if (slide.value == 1 && props.carousel) {
+    slide.value = slides.value + 1
+    setTimeout(() => {
+      transitionTime.value = 0
+      translateX.value = width * slides.value * -1
+    }, 420)
+  } else if (slide.value == slides.value + 2 && props.carousel) {
+    slide.value = 2
+    setTimeout(() => {
+      transitionTime.value = 0
+      translateX.value = -width
+    }, 420)
+  }
+  if (slide.value < 1 && !props.carousel) {
+    slide.value = 1
+    translateX.value = 0
+  } else if (slide.value > slides.value && !props.carousel) {
+    slide.value = slides.value
+    translateX.value = width * (slides.value - 1) * -1
+  }
+})
 </script>
 
 <style scoped lang="scss">
@@ -37,26 +185,40 @@
   &__container {
     position: relative;
 
-    padding: 0 69px;
-
     width: 100%;
-    height: 50vh;
+
+    overflow: hidden;
+  }
+
+  &__content {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    transition-property: transform;
   }
 
   &__pagination {
     position: absolute;
     bottom: 60px;
+    left: 50%;
+
+    transform: translateX(-50%);
 
     display: grid;
-    grid-template-columns: 20% 60% 20%;
+    grid-template-columns: 15% 70% 15%;
 
-    width: 70%;
+    width: 90%;
+
+    z-index: 2;
   }
 
   &__pagination-list {
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
 
     color: $white;
   }
@@ -65,7 +227,11 @@
     width: 19px;
     height: 19px;
 
-    margin-right: 26px;
+    margin: 0 21px;
+
+    font-family: 'Montserrat', sans-serif;
+    font-size: 15px;
+    font-weight: 600;
 
     &--active {
       background-color: $white;
@@ -82,6 +248,8 @@
 
     width: 48px;
     height: 48px;
+
+    background: transparent;
 
     border: 1px solid rgba(255 255 255 / 10%);
     border-radius: 50%;
